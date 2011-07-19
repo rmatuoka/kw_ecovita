5b84154ad9a2b51bd1ee6ef86d5b4973086c404e
Rafael Matuoka <rmatuoka@korewa.com.br>
2011-07-19 14:59:17 -0300

    CORRECAO

--- Si/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-KwEcovita::Application.config.secret_token = '8e26afe4d947fbdd35262137a636cae9b06daff017d3af3bf058f867c213b1b1ce963b5c703d42b788ae93a700cccc3a98f41d8222872bebe700a250c5802777'
