02bc761353228362331fce5a9ed5bfabd2c4cc92
-_- Zaba -_- <bsilva@korewa.com.br>
2012-01-13 18:27:58 -0200

    Ajustes da Lista

--- app/controllers/product_comments_controller.rb
+++ app/controllers/product_comments_controller.rb
@@ -6,6 +6,8 @@ class ProductCommentsController < ApplicationController
   
   def create
     @comentario = ProductComment.new(params[:product_comment])
+    @comentario.name = current_user.nome
+    @comentario.email = current_user.email
     @comentario.save
     redirect_to produto_path(params[:product_comment][:product_id])
   end
