2e397826faf18d57cb86da2d0ff50e6ce940e561
Rafael Matuoka <rmatuoka@korewa.com.br>
2012-01-20 19:44:17 -0200

    ajuste foto

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,5 +1,5 @@
 class ProductImage < ActiveRecord::Base
   belongs_to :product
   
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x128>" }
+  has_attached_file :image, :styles => { :medium => "290x290>", :thumb => "100x128>" }
 end
